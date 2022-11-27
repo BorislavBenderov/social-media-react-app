3a0b42010be58e0d4ac7b8568ad73881f77b8808
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-11-27 23:14:07 +0200

    Add Footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Login } from './components/auth/Login';
 import { Register } from './components/auth/Register';
+import { Footer } from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Register />
+      <Footer />
     </div>
   );
 }
