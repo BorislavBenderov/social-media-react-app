3a4584bba04e69b07bb9a8f171857f5ff860af44
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-11-27 23:12:31 +0200

    Add Register component

---
+++ src/components/auth/Register.js
@@ -0,0 +1,33 @@
+export const Register = () => {
+    return (
+        <div className="auth">
+            <div className="auth__container">
+                <h1>Social Media</h1>
+                <form className="auth__form">
+                    <label htmlfor="email" />
+                    <input type="text" placeholder="Email" id="email" name="email" />
+                    <label htmlfor="password" />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        id="password"
+                        name="password"
+                    />
+                    <label htmlfor="repeatPassword" />
+                    <input
+                        type="password"
+                        placeholder="Repeat Password"
+                        id="repeatPassword"
+                        name="repeatPassword"
+                    />
+                    <button type="submit">Register</button>
+                </form>
+            </div>
+            <div className="auth__action">
+                <p>Have an account?</p>
+                <a href="">Log in</a>
+            </div>
+        </div>
+
+    );
+}
