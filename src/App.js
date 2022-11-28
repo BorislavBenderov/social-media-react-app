fdfd821bd1945122e3543e908738fc8deb509674
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-11-28 11:15:53 +0200

    Add Posts components jsx and css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Login } from './components/auth/Login';
 import { Register } from './components/auth/Register';
 import { Footer } from './components/footer/Footer';
+import { Posts } from './components/Posts/Posts';
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      <Posts />
       <Footer />
     </div>
   );
