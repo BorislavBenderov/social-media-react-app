4f173dfb4ca3fe023a817b8cd3ec3f7e571ee67b
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-11-28 12:19:41 +0200

    Add firebase to project

---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC-mLAJJYunkKmgVi0-hQEGPE5S22TUvU0",
+  authDomain: "social-media-react-app-401bc.firebaseapp.com",
+  projectId: "social-media-react-app-401bc",
+  storageBucket: "social-media-react-app-401bc.appspot.com",
+  messagingSenderId: "16754577876",
+  appId: "1:16754577876:web:d488e411b5b57d65060641"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app);
