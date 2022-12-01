899aee4f01244e28b859cf421395052cee69a7eb
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-01 10:17:08 +0200

    Add unique username to every single user in register

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -3,9 +3,14 @@ import { setPersistence, browserSessionPersistence, createUserWithEmailAndPasswo
 import { setDoc, doc } from 'firebase/firestore';
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import { auth, database, storage } from '../../firebaseConfig';
+import { useContext, useState } from 'react';
+import { UserContext } from '../../contexts/UserContext';
 
 export const Register = () => {
+    const [isFileAdd, setIsFileAdd] = useState(false);
     const navigate = useNavigate();
+    const { users } = useContext(UserContext);
+    console.log(users)
 
     const onRegister = (e) => {
         e.preventDefault();
@@ -13,9 +18,12 @@ export const Register = () => {
         const formData = new FormData(e.target);
 
         const email = formData.get('email');
+        const username = formData.get('username');
         const password = formData.get('password');
         const repeatPassword = formData.get('repeatPassword');
         const image = formData.get('image');
+
+        const isUsernameInUse = users.find(user => user.displayName === username);
 
         if (email === '' || password === '' || repeatPassword === '' || image.name === '') {
             alert('Please fill all the fields!');
@@ -23,7 +31,12 @@ export const Register = () => {
         }
 
         if (password !== repeatPassword) {
-            alert("Your password and confirmation password do not match");
+            alert("Your password and confirmation password do not match!");
+            return;
+        }
+
+        if (isUsernameInUse) {
+            alert('This username is already in use!');
             return;
         }
 
@@ -42,21 +55,24 @@ export const Register = () => {
                         getDownloadURL(uploadTask.snapshot.ref)
                             .then((downloadUrl) => {
                                 updateProfile(res.user, {
-                                    displayName: email,
+                                    displayName: username,
                                     photoURL: downloadUrl
                                 });
                                 setDoc(doc(database, 'users', res.user.uid), {
-                                    displayName: email,
+                                    email: email,
+                                    displayName: username,
                                     image: downloadUrl,
-                                    uid: res.user.uid
+                                    uid: res.user.uid,
+                                    following: [],
+                                    followers: []
                                 })
                                 navigate('/posts')
                             })
-                            .catch((err) => {
-                                alert(err.message);
-                            });
                     })
             })
+            .catch((err) => {
+                alert(err.message);
+            });
     }
 
 
@@ -67,6 +83,8 @@ export const Register = () => {
                 <form className="auth__form" onSubmit={onRegister}>
                     <label htmlFor="email" />
                     <input type="text" placeholder="Email" id="email" name="email" />
+                    <label htmlFor="username" />
+                    <input type="text" placeholder="Username" id="username" name="username" />
                     <label htmlFor="password" />
                     <input
                         type="password"
@@ -81,12 +99,19 @@ export const Register = () => {
                         id="repeatPassword"
                         name="repeatPassword"
                     />
-                    <label htmlFor="image" />
+                    <label htmlFor="image">
+                        <i
+                            className="fa fa-picture-o fa-lg"
+                            aria-hidden="true"
+                            style={{ cursor: "pointer" }}>
+                            {isFileAdd ? "File is added!" : "Add a file"}</i>
+                    </label>
                     <input
                         type="file"
                         placeholder="Choose a file"
                         id="image"
                         name="image"
+                        onChange={() => setIsFileAdd(true)}
                     />
                     <button type="submit">Register</button>
                 </form>
