2ecabfdd9fa9b0f941c68824aa465877afad4265
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-12 08:47:41 +0200

    Render posts only on users who logged user follow

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,14 +1,21 @@
 import { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
 import { PostContext } from '../../contexts/PostContext';
+import { UserContext } from '../../contexts/UserContext';
 import { Post } from './Post';
 
 export const Posts = () => {
-    const { posts } = useContext(PostContext);   
+    const { posts } = useContext(PostContext);
+    const { users } = useContext(UserContext);
+    const { loggedUser } = useContext(AuthContext);
+    const currentUsersPosts = posts.filter(post => post.ownerId === loggedUser.uid);
+    const followedUsers = users.filter(user => user.followers.includes(loggedUser.uid));
+    const followedUsersPosts = posts.filter(post => followedUsers.find(user => user.uid === post.ownerId)).concat(currentUsersPosts);
 
     return (
         <main>
             <section className="content__container">
-                {posts.map(post => <Post key={post.id} post={post} />)}
+                {followedUsersPosts.map(post => <Post key={post.id} post={post} />)}
             </section>
         </main>
     );
