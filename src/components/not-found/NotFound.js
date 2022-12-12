97a488f8c53be3f1feae84e054b81649bbd74850
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-12 12:26:19 +0200

    Add go home option on 404 page

--- src/components/not-found/NotFound.js
+++ src/components/not-found/NotFound.js
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
+
 export const NotFound = () => {
     return (
         <div className="not-found">
             <div className="not-found-info">
                 <h2>404.That's an error.</h2>
                 <p>The requested page was not found on this server.</p>
+                <Link to='/posts'>
+                    <p>Go Home</p>
+                    <i className="fa fa-home fa-lg" aria-hidden="true"></i>
+                </Link>
             </div>
         </div>
     );
