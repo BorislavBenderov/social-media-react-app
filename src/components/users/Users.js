538a5d72160b4b14c2445fa29cea58ce414d34c1
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-12 09:18:01 +0200

    Add Users component and render users to follow in him

---
+++ src/components/users/Users.js
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { UserContext } from '../../contexts/UserContext';
+import { AuthContext } from '../../contexts/AuthContext';
+import { UsersContainer } from "../user-profile/UsersContainer";
+
+export const Users = () => {
+    const { users } = useContext(UserContext);
+    const { loggedUser } = useContext(AuthContext);
+    const usersToFollow = users.filter(user => !user.followers.includes(loggedUser.uid)).filter(user => user.uid !== loggedUser.uid);
+
+    return (
+        <div className="messanger__cont">
+            <section className="messanger__allusers">
+                <div className="messanger__curentuser">
+                    <h2>Users To Follow</h2>
+                </div>
+                {usersToFollow.length > 0
+                    ? usersToFollow.map(user => <UsersContainer key={user.id} user={user} />)
+                    : <p className="users__follow__chat">There are no users to follow!</p>}
+            </section>
+        </div>
+    );
+}
