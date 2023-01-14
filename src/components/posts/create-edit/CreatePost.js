8b85d4c9eb4bf5965d80105fa56dc293cab74f44
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-14 10:30:59 +0200

    Add loading state in Create and Edit Post

--- src/components/posts/create-edit/CreatePost.js
+++ src/components/posts/create-edit/CreatePost.js
@@ -1,90 +1,95 @@
-import { v4 as uuidv4 } from 'uuid';
-import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
-import { database, storage } from '../../../firebaseConfig';
-import { useContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../../contexts/AuthContext';
+import { v4 as uuidv4 } from "uuid";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { database, storage } from "../../../firebaseConfig";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../../contexts/AuthContext";
 
 export const CreatePost = () => {
-    const [err, setErr] = useState('');
-    const [isFileAdd, setIsFileAdd] = useState(false);
-    const { loggedUser } = useContext(AuthContext);
-    const navigate = useNavigate();
+  const [err, setErr] = useState("");
+  const [isFileAdd, setIsFileAdd] = useState(false);
+  const { loggedUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
-    const onCreate = (e) => {
-        e.preventDefault();
+  const onCreate = (e) => {
+    e.preventDefault();
 
-        const formData = new FormData(e.target);
+    const formData = new FormData(e.target);
 
-        const image = formData.get('image');
-        const description = formData.get('description');
+    const image = formData.get("image");
+    const description = formData.get("description");
 
-        if (image.name === '') {
-            setErr('Please add a file!');
-            return;
-        }
-
-        const storageRef = ref(storage, `/posts/${uuidv4()}`);
-        const uploadTask = uploadBytesResumable(storageRef, image);
-        uploadTask.on('state_changed',
-            (snapshot) => {
-            },
-            (error) => {
-                setErr(error.message)
-            },
-            () => {
-                getDownloadURL(uploadTask.snapshot.ref)
-                    .then((downloadURL) => {
-                        const postData = {
-                            description,
-                            image: downloadURL,
-                            timestamp: serverTimestamp(),
-                            ownerId: loggedUser.uid,
-                            likes: [],
-                        }
-                        addDoc(collection(database, 'posts'), postData)
-                            .then(() => {
-                                navigate('/posts');
-                            })
-                            .catch((err) => {
-                                setErr(err.message);
-                            })
-                    });
-            }
-        );
+    if (image.name === "") {
+      setErr("Please add a file!");
+      return;
     }
-
-    return (
-        <div className="auth">
-            <div className="auth__container">
-                <h1>Create New Post</h1>
-                <form className="auth__form" onSubmit={onCreate}>
-                    <label htmlFor="description" />
-                    <textarea
-                        className='textarea__post'
-                        type="text"
-                        placeholder="Write a description..."
-                        id="description"
-                        name="description" />
-                    <label htmlFor="image">
-                        <i
-                            className="fa fa-picture-o fa-lg"
-                            aria-hidden="true"
-                            style={{ cursor: "pointer" }}>
-                            {isFileAdd ? "File is added!" : "Add a file"}</i>
-                    </label>
-                    <input
-                        type="file"
-                        placeholder="Choose a picture"
-                        id="image"
-                        name="image"
-                        onChange={() => setIsFileAdd(true)}
-                    />
-                    <p className="errors">{err}</p>
-                    <button type="submit">Create</button>
-                </form>
-            </div>
-        </div>
+    setLoading(true);
+    const storageRef = ref(storage, `/posts/${uuidv4()}`);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {},
+      (error) => {
+        setErr(error.message);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          const postData = {
+            description,
+            image: downloadURL,
+            timestamp: serverTimestamp(),
+            ownerId: loggedUser.uid,
+            likes: [],
+          };
+          addDoc(collection(database, "posts"), postData)
+            .then(() => {
+              setLoading(false);
+              navigate("/posts");
+            })
+            .catch((err) => {
+              setErr(err.message);
+              setLoading(false);
+            });
+        });
+      }
     );
-}
+  };
+
+  return (
+    <div className="auth">
+      <div className="auth__container">
+        <h1>Create New Post</h1>
+        <form className="auth__form" onSubmit={onCreate}>
+          <label htmlFor="description" />
+          <textarea
+            className="textarea__post"
+            type="text"
+            placeholder="Write a description..."
+            id="description"
+            name="description"
+          />
+          <label htmlFor="image">
+            <i
+              className="fa fa-picture-o fa-lg"
+              aria-hidden="true"
+              style={{ cursor: "pointer" }}
+            >
+              {isFileAdd ? "File is added!" : "Add a file"}
+            </i>
+          </label>
+          <input
+            type="file"
+            placeholder="Choose a picture"
+            id="image"
+            name="image"
+            onChange={() => setIsFileAdd(true)}
+          />
+          <p className="errors">{err}</p>
+          <button type="submit">{loading ? 'Loading...': 'Create'}</button>
+        </form>
+      </div>
+    </div>
+  );
+};
--- src/components/posts/create-edit/EditPost.js
+++ src/components/posts/create-edit/EditPost.js
@@ -5,51 +5,56 @@ import { PostContext } from "../../../contexts/PostContext";
 import { database } from "../../../firebaseConfig";
 
 export const EditPost = () => {
-    const [err, setErr] = useState('');
-    const { posts } = useContext(PostContext);
-    const { postId } = useParams();
-    const navigate = useNavigate();
-    const currentPost = posts.find(post => post.id === postId);
+  const [err, setErr] = useState("");
+  const { posts } = useContext(PostContext);
+  const { postId } = useParams();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const currentPost = posts.find((post) => post.id === postId);
 
-    const [value, setValue] = useState({
-        description: currentPost?.description
-    });
+  const [value, setValue] = useState({
+    description: currentPost?.description,
+  });
 
-    const onEditPost = (e) => {
-        e.preventDefault();
+  const onEditPost = (e) => {
+    e.preventDefault();
 
-        const formData = new FormData(e.target);
-        const description = formData.get('description');
+    const formData = new FormData(e.target);
+    const description = formData.get("description");
 
-        updateDoc(doc(database, 'posts', postId), {
-            description
-        })
-            .then(() => {
-                navigate('/posts');
-            })
-            .catch((err) => {
-                setErr(err.message);
-            })
-    }
+    setLoading(true);
+    updateDoc(doc(database, "posts", postId), {
+      description,
+    })
+      .then(() => {
+        navigate("/posts");
+        setLoading(false);
+      })
+      .catch((err) => {
+        setErr(err.message);
+        setLoading(false);
+      });
+  };
 
-    return (
-        <div className="auth">
-            <div className="auth__container">
-                <h1>Edit Post Description</h1>
-                <form className="auth__form" onSubmit={onEditPost}>
-                    <label htmlFor="description" />
-                    <textarea
-                        className='textarea__post'
-                        type="text"
-                        placeholder="Write a description..."
-                        id="description"
-                        name="description"
-                        value={value.description}
-                        onChange={(e) => setValue(e.target.value)} />
-                    <button type="submit">Edit</button>
-                    <p className="errors">{err}</p>
-                </form>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="auth">
+      <div className="auth__container">
+        <h1>Edit Post Description</h1>
+        <form className="auth__form" onSubmit={onEditPost}>
+          <label htmlFor="description" />
+          <textarea
+            className="textarea__post"
+            type="text"
+            placeholder="Write a description..."
+            id="description"
+            name="description"
+            value={value.description}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <button type="submit">{loading ? "Loading" : "Edit"}</button>
+          <p className="errors">{err}</p>
+        </form>
+      </div>
+    </div>
+  );
+};
