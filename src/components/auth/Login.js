0d1b55a85779e6ccd88b1a69813fe911f689e82c
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-14 10:25:45 +0200

    Add loading state in Login and Register

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,60 +1,66 @@
-import { Link, useNavigate } from 'react-router-dom';
-import { setPersistence, signInWithEmailAndPassword, browserSessionPersistence } from 'firebase/auth';
-import { auth } from '../../firebaseConfig';
-import { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import {
+  setPersistence,
+  signInWithEmailAndPassword,
+  browserSessionPersistence,
+} from "firebase/auth";
+import { auth } from "../../firebaseConfig";
+import { useState } from "react";
 
 export const Login = () => {
-    const [err, setErr] = useState('');
-    const navigate = useNavigate();
+  const [err, setErr] = useState("");
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
-    const onLogin = (e) => {
-        e.preventDefault();
+  const onLogin = (e) => {
+    e.preventDefault();
 
-        const formData = new FormData(e.target);
+    const formData = new FormData(e.target);
 
-        const email = formData.get('email');
-        const password = formData.get('password');
+    const email = formData.get("email");
+    const password = formData.get("password");
 
-        if (email === '' || password === '') {
-            setErr('Please fill all the fields!');
-            return;
-        }
-
-        setPersistence(auth, browserSessionPersistence)
-            .then(() => {
-                signInWithEmailAndPassword(auth, email, password)
-                    .then(() => {
-                        navigate('/posts');
-                    })
-                    .catch((err) => {
-                        setErr(err.message);
-                    })
-            })
-
+    if (email === "" || password === "") {
+      setErr("Please fill all the fields!");
+      return;
     }
 
-    return (
-        <div className="auth">
-            <div className="auth__container">
-                <h1>Social Media</h1>
-                <form className="auth__form" onSubmit={onLogin}>
-                    <label htmlFor="email" />
-                    <input type="text" placeholder="Email" id="email" name="email" />
-                    <label htmlFor="password" />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        id="password"
-                        name="password"
-                    />
-                    <button type="submit">Log In</button>
-                    <p className="errors">{err}</p>
-                </form>
-            </div>
-            <div className="auth__action">
-                <p>Don't have an account?</p>
-                <Link to="/register">Sign up</Link>
-            </div>
-        </div>
-    );
-}
+    setPersistence(auth, browserSessionPersistence).then(() => {
+      setLoading(true);
+      signInWithEmailAndPassword(auth, email, password)
+        .then(() => {
+          setLoading(false);
+          navigate("/posts");
+        })
+        .catch((err) => {
+          setLoading(false);
+          setErr(err.message);
+        });
+    });
+  };
+
+  return (
+    <div className="auth">
+      <div className="auth__container">
+        <h1>Social Media</h1>
+        <form className="auth__form" onSubmit={onLogin}>
+          <label htmlFor="email" />
+          <input type="text" placeholder="Email" id="email" name="email" />
+          <label htmlFor="password" />
+          <input
+            type="password"
+            placeholder="Password"
+            id="password"
+            name="password"
+          />
+          <button type="submit">{loading ? "Loading..." : "Log In"}</button>
+          <p className="errors">{err}</p>
+        </form>
+      </div>
+      <div className="auth__action">
+        <p>Don't have an account?</p>
+        <Link to="/register">Sign up</Link>
+      </div>
+    </div>
+  );
+};
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,131 +1,151 @@
-import { Link, useNavigate } from 'react-router-dom';
-import { setPersistence, browserSessionPersistence, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
-import { setDoc, doc } from 'firebase/firestore';
-import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
-import { auth, database, storage } from '../../firebaseConfig';
-import { useContext, useState } from 'react';
-import { UserContext } from '../../contexts/UserContext';
+import { Link, useNavigate } from "react-router-dom";
+import {
+  setPersistence,
+  browserSessionPersistence,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { setDoc, doc } from "firebase/firestore";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { auth, database, storage } from "../../firebaseConfig";
+import { useContext, useState } from "react";
+import { UserContext } from "../../contexts/UserContext";
 
 export const Register = () => {
-    const [err, setErr] = useState('');
-    const [isFileAdd, setIsFileAdd] = useState(false);
-    const navigate = useNavigate();
-    const { users } = useContext(UserContext);
+  const [err, setErr] = useState("");
+  const [isFileAdd, setIsFileAdd] = useState(false);
+  const navigate = useNavigate();
+  const { users } = useContext(UserContext);
+  const [loading, setLoading] = useState(false);
 
-    const onRegister = (e) => {
-        e.preventDefault();
+  const onRegister = (e) => {
+    e.preventDefault();
 
-        const formData = new FormData(e.target);
+    const formData = new FormData(e.target);
 
-        const email = formData.get('email');
-        const username = formData.get('username');
-        const password = formData.get('password');
-        const repeatPassword = formData.get('repeatPassword');
-        const image = formData.get('image');
+    const email = formData.get("email");
+    const username = formData.get("username");
+    const password = formData.get("password");
+    const repeatPassword = formData.get("repeatPassword");
+    const image = formData.get("image");
 
-        const isUsernameInUse = users.find(user => user.displayName === username);
+    const isUsernameInUse = users.find((user) => user.displayName === username);
 
-        if (email === '' || password === '' || repeatPassword === '' || image.name === '') {
-            setErr('Please fill all the fields!');
-            return;
-        }
-
-        if (password !== repeatPassword) {
-            setErr("Your password and confirmation password do not match!");
-            return;
-        }
-
-        if (isUsernameInUse) {
-            setErr('This username is already in use!');
-            return;
-        }
-
-        if (username.length < 2 || username.length > 10) {
-            setErr('Username must be more than 2 characters and less then 10!');
-            return;
-        }
-
-        setPersistence(auth, browserSessionPersistence)
-            .then(async () => {
-                const res = await createUserWithEmailAndPassword(auth, email, password);
-                const storageRef = ref(storage, `/users/${email}`);
-                const uploadTask = uploadBytesResumable(storageRef, image);
-                uploadTask.on('state_changed',
-                    (snapshot) => {
-                    },
-                    (err) => {
-                        alert(err.message);
-                    },
-                    () => {
-                        getDownloadURL(uploadTask.snapshot.ref)
-                            .then((downloadUrl) => {
-                                updateProfile(res.user, {
-                                    displayName: username,
-                                    photoURL: downloadUrl
-                                });
-                                setDoc(doc(database, 'users', res.user.uid), {
-                                    email: email,
-                                    displayName: username,
-                                    image: downloadUrl,
-                                    uid: res.user.uid,
-                                    following: [],
-                                    followers: []
-                                })
-                                navigate('/posts')
-                            })
-                    })
-            })
-            .catch((err) => {
-                setErr(err.message);
-            });
+    if (
+      email === "" ||
+      password === "" ||
+      repeatPassword === "" ||
+      image.name === ""
+    ) {
+      setErr("Please fill all the fields!");
+      return;
     }
 
+    if (password !== repeatPassword) {
+      setErr("Your password and confirmation password do not match!");
+      return;
+    }
 
-    return (
-        <div className="auth">
-            <div className="auth__container">
-                <h1>Social Media</h1>
-                <form className="auth__form" onSubmit={onRegister}>
-                    <label htmlFor="email" />
-                    <input type="text" placeholder="Email" id="email" name="email" />
-                    <label htmlFor="username" />
-                    <input type="text" placeholder="Username" id="username" name="username" />
-                    <label htmlFor="password" />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        id="password"
-                        name="password"
-                    />
-                    <label htmlFor="repeatPassword" />
-                    <input
-                        type="password"
-                        placeholder="Repeat Password"
-                        id="repeatPassword"
-                        name="repeatPassword"
-                    />
-                    <label htmlFor="image">
-                        <i
-                            className="fa fa-picture-o fa-lg"
-                            aria-hidden="true"
-                            style={{ cursor: "pointer" }}>
-                            {isFileAdd ? "File is added!" : "Add a file"}</i>
-                    </label>
-                    <input
-                        type="file"
-                        placeholder="Choose a file"
-                        id="image"
-                        name="image"
-                        onChange={() => setIsFileAdd(true)}
-                    />
-                    <button type="submit">Register</button>
-                    <p className="errors">{err}</p>
-                </form>
-            </div>
-            <div className="auth__action">
-                <p>Have an account?</p>
-                <Link to="/">Log in</Link>
-            </div>
-        </div>
-    );
-}
+    if (isUsernameInUse) {
+      setErr("This username is already in use!");
+      return;
+    }
+
+    if (username.length < 2 || username.length > 10) {
+      setErr("Username must be more than 2 characters and less then 10!");
+      return;
+    }
+
+    setPersistence(auth, browserSessionPersistence)
+      .then(async () => {
+        setLoading(true);
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const storageRef = ref(storage, `/users/${email}`);
+        const uploadTask = uploadBytesResumable(storageRef, image);
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {},
+          (err) => {
+            alert(err.message);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
+              updateProfile(res.user, {
+                displayName: username,
+                photoURL: downloadUrl,
+              });
+              setDoc(doc(database, "users", res.user.uid), {
+                email: email,
+                displayName: username,
+                image: downloadUrl,
+                uid: res.user.uid,
+                following: [],
+                followers: [],
+              });
+              setLoading(false);
+              navigate("/posts");
+            });
+          }
+        );
+      })
+      .catch((err) => {
+        setErr(err.message);
+        setLoading(false);
+      });
+  };
+
+  return (
+    <div className="auth">
+      <div className="auth__container">
+        <h1>Social Media</h1>
+        <form className="auth__form" onSubmit={onRegister}>
+          <label htmlFor="email" />
+          <input type="text" placeholder="Email" id="email" name="email" />
+          <label htmlFor="username" />
+          <input
+            type="text"
+            placeholder="Username"
+            id="username"
+            name="username"
+          />
+          <label htmlFor="password" />
+          <input
+            type="password"
+            placeholder="Password"
+            id="password"
+            name="password"
+          />
+          <label htmlFor="repeatPassword" />
+          <input
+            type="password"
+            placeholder="Repeat Password"
+            id="repeatPassword"
+            name="repeatPassword"
+          />
+          <label htmlFor="image">
+            <i
+              className="fa fa-picture-o fa-lg"
+              aria-hidden="true"
+              style={{ cursor: "pointer" }}
+            >
+              {isFileAdd ? "File is added!" : "Add a file"}
+            </i>
+          </label>
+          <input
+            type="file"
+            placeholder="Choose a file"
+            id="image"
+            name="image"
+            onChange={() => setIsFileAdd(true)}
+          />
+          <button type="submit">{loading ? "Loading..." : "Register"}</button>
+          <p className="errors">{err}</p>
+        </form>
+      </div>
+      <div className="auth__action">
+        <p>Have an account?</p>
+        <Link to="/">Log in</Link>
+      </div>
+    </div>
+  );
+};
