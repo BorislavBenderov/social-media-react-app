edc6eaa05ff390f38190b4843b77e4a17c20812c
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-14 10:34:40 +0200

    Add loading state in EditUser component

--- src/components/user-profile/EditUser.js
+++ src/components/user-profile/EditUser.js
@@ -8,126 +8,152 @@ import { AuthContext } from "../../contexts/AuthContext";
 import { UserContext } from "../../contexts/UserContext";
 
 export const EditUser = () => {
-    const { users } = useContext(UserContext);
-    const { loggedUser } = useContext(AuthContext);
-    const { userId } = useParams();
-    const navigate = useNavigate();
-    const [err, setErr] = useState('');
-    const currentUser = users.find(user => user.uid === userId);
-    const [values, setValues] = useState({
-        username: currentUser?.displayName,
-        description: currentUser?.profileDescription,
-        image: currentUser?.image
-    });
+  const { users } = useContext(UserContext);
+  const { loggedUser } = useContext(AuthContext);
+  const { userId } = useParams();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [err, setErr] = useState("");
+  const currentUser = users.find((user) => user.uid === userId);
+  const [values, setValues] = useState({
+    username: currentUser?.displayName,
+    description: currentUser?.profileDescription,
+    image: currentUser?.image,
+  });
 
-    const changeHandler = (e) => {
-        setValues(oldState => ({
-            ...oldState,
-            [e.target.name]: e.target.value
-        }));
+  const changeHandler = (e) => {
+    setValues((oldState) => ({
+      ...oldState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onEditUser = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const description = formData.get("description");
+    const image = formData.get("image");
+
+    const isUsernameInUse = users
+      .filter((user) => user.uid !== loggedUser.uid)
+      .find((user) => user.displayName === username);
+
+    if (isUsernameInUse) {
+      alert("This username is already in use!");
+      return;
     }
 
-    const onEditUser = (e) => {
-        e.preventDefault();
+    if (username === "") {
+      alert("Please set a username!");
+      return;
+    }
 
-        const formData = new FormData(e.target);
+    if (username.length < 2 || username.length > 10) {
+      alert("Username must be more than 2 characters and less then 10!");
+      return;
+    }
 
-        const username = formData.get('username');
-        const description = formData.get('description');
-        const image = formData.get('image');
-
-        const isUsernameInUse = users.filter(user => user.uid !== loggedUser.uid).find(user => user.displayName === username);
-
-        if (isUsernameInUse) {
-            alert('This username is already in use!');
-            return;
-        }
-
-        if (username === '') {
-            alert('Please set a username!');
-            return;
-        }
-
-        if (username.length < 2 || username.length > 10) {
-            alert('Username must be more than 2 characters and less then 10!');
-            return;
-        }
-
-        if (image.name === '') {
-            updateProfile(loggedUser, {
-                photoURL: currentUser?.image,
-                displayName: username
-            });
-            updateDoc(doc(database, 'users', loggedUser.uid), {
-                image: currentUser?.image,
+    setLoading(true);
+    if (image.name === "") {
+      updateProfile(loggedUser, {
+        photoURL: currentUser?.image,
+        displayName: username,
+      });
+      updateDoc(doc(database, "users", loggedUser.uid), {
+        image: currentUser?.image,
+        displayName: username,
+        profileDescription: description,
+      })
+        .then(() => {
+          navigate(`/profile/${loggedUser.uid}`);
+          setLoading(false);
+        })
+        .catch((err) => {
+          alert(err.message);
+          setLoading(false);
+        });
+    } else {
+      const storageRef = ref(storage, `/users/${loggedUser?.email}`);
+      const uploadTask = uploadBytesResumable(storageRef, image);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {},
+        (err) => {
+          alert(err.message);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref)
+            .then((downloadUrl) => {
+              updateProfile(loggedUser, {
+                photoURL: downloadUrl,
                 displayName: username,
-                profileDescription: description
+              });
+              updateDoc(doc(database, "users", loggedUser.uid), {
+                image: downloadUrl,
+                displayName: username,
+                profileDescription: description,
+              }).then(() => {
+                navigate(`/profile/${loggedUser.uid}`);
+                setLoading(false);
+              });
             })
-                .then(() => {
-                    navigate(`/profile/${loggedUser.uid}`);
-                })
-                .catch((err) => {
-                    alert(err.message);
-                })
-        } else {
-            const storageRef = ref(storage, `/users/${loggedUser?.email}`);
-            const uploadTask = uploadBytesResumable(storageRef, image);
-            uploadTask.on('state_changed',
-                (snapshot) => {
-                },
-                (err) => {
-                    alert(err.message);
-                },
-                () => {
-                    getDownloadURL(uploadTask.snapshot.ref)
-                        .then((downloadUrl) => {
-                            updateProfile(loggedUser, {
-                                photoURL: downloadUrl,
-                                displayName: username
-                            });
-                            updateDoc(doc(database, 'users', loggedUser.uid), {
-                                image: downloadUrl,
-                                displayName: username,
-                                profileDescription: description
-                            })
-                                .then(() => {
-                                    navigate(`/profile/${loggedUser.uid}`);
-                                })
-                        })
-                        .catch((err) => {
-                            setErr(err.message);
-                        })
-                })
+            .catch((err) => {
+              setErr(err.message);
+              setLoading(false);
+            });
         }
+      );
     }
+  };
 
-    return (
-        <div className="auth">
-            <div className="auth__container">
-                <h1>Edit User</h1>
-                <form className="auth__form" onSubmit={onEditUser}>
-                    <label htmlFor="username" />
-                    <input type="text" placeholder="Username" id="username" name="username" onChange={changeHandler} value={values.username} />
-                    <label htmlFor="description" />
-                    <textarea className='textarea__post' type="text" placeholder="Description" id="description" name="description" value={values.description} onChange={changeHandler} />
-                    <label htmlFor="image">
-                        <i
-                            className="fa fa-picture-o fa-lg"
-                            aria-hidden="true"
-                            style={{ cursor: "pointer" }}>File</i>
-                    </label>
-                    <input
-                        type="file"
-                        placeholder="Choose a file"
-                        id="image"
-                        name="image"
-                        value={values.image?.name}
-                        onChange={changeHandler}
-                    />
-                    <button type="submit">Edit</button>
-                    <p className="errors">{err}</p>
-                </form>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="auth">
+      <div className="auth__container">
+        <h1>Edit User</h1>
+        <form className="auth__form" onSubmit={onEditUser}>
+          <label htmlFor="username" />
+          <input
+            type="text"
+            placeholder="Username"
+            id="username"
+            name="username"
+            onChange={changeHandler}
+            value={values.username}
+          />
+          <label htmlFor="description" />
+          <textarea
+            className="textarea__post"
+            type="text"
+            placeholder="Description"
+            id="description"
+            name="description"
+            value={values.description}
+            onChange={changeHandler}
+          />
+          <label htmlFor="image">
+            <i
+              className="fa fa-picture-o fa-lg"
+              aria-hidden="true"
+              style={{ cursor: "pointer" }}
+            >
+              File
+            </i>
+          </label>
+          <input
+            type="file"
+            placeholder="Choose a file"
+            id="image"
+            name="image"
+            value={values.image?.name}
+            onChange={changeHandler}
+          />
+          <button type="submit">{loading ? "Loading..." : "Edit"}</button>
+          <p className="errors">{err}</p>
+        </form>
+      </div>
+    </div>
+  );
+};
